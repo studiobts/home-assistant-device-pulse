466d60008f1b616664e90c95a7a35decbccd5f5c
studiobts <info@studiobts.it>
2025-11-19 08:28:16 +0000

    Add `get_devices` websocket command and enhance binary sensor with state restoration and attributes

--- custom_components/device_pulse/entities/binary_sensor.py
+++ custom_components/device_pulse/entities/binary_sensor.py
@@ -2,21 +2,44 @@
 
 import logging
 
-from custom_components.device_pulse.const import ENTITY_TAG_PING_STATUS
+from custom_components.device_pulse.const import (
+    ENTITY_ATTR_STATE_SINCE,
+    ENTITY_ATTR_PINGS_FAILED,
+    ENTITY_TAG_PING_STATUS
+)
 
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
     BinarySensorEntity,
 )
+from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.util import dt as dt_util
 
 from .base import BaseCoordinatorEntity
 
-
-
 _LOGGER = logging.getLogger(__name__)
 
-class DevicePingStatusBinarySensor(BaseCoordinatorEntity, BinarySensorEntity):
+class DevicePingStatusBinarySensor(BaseCoordinatorEntity, BinarySensorEntity, RestoreEntity):
     """Binary sensor for device ping status."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the sensor."""
+        super().__init__(*args, **kwargs)
+
+        self._state_since = None
+        self._previous_state = None
+
+    async def async_added_to_hass(self):
+        """Run when entity about to be added to hass."""
+        await super().async_added_to_hass()
+
+        # Restore previous state
+        if last_state := await self.async_get_last_state():
+            self._previous_state = last_state.state == "on"
+            self._state_since = last_state.attributes.get(ENTITY_ATTR_STATE_SINCE, None)
+        # Initialize state since if None
+        if self._state_since is None:
+            self._state_since = dt_util.now().timestamp()
 
     @property
     def _tag(self) -> str:
@@ -32,8 +55,26 @@ class DevicePingStatusBinarySensor(BaseCoordinatorEntity, BinarySensorEntity):
         """Additional initialization for the sensor."""
         self._attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
 
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        current_state = self.coordinator.data.is_alive
+
+        if self._previous_state is not None and self._previous_state != current_state:
+            self._state_since = dt_util.now().timestamp()
+
+        self._previous_state = current_state
+        super()._handle_coordinator_update()
+
     @property
     def is_on(self) -> bool:
         """Return true if the device is reachable."""
         return self.coordinator.data.is_alive
 
+    @property
+    def extra_state_attributes(self) -> dict:
+        """Return additional attributes."""
+        return {
+            **super().extra_state_attributes,
+            ENTITY_ATTR_STATE_SINCE: self._state_since,
+            ENTITY_ATTR_PINGS_FAILED: self.coordinator.failed_pings > 0
+        }
--- custom_components/device_pulse/websocket_api.py
+++ custom_components/device_pulse/websocket_api.py
@@ -7,22 +7,39 @@ from datetime import timedelta
 from homeassistant.components.recorder import get_instance
 from homeassistant.components.recorder.util import session_scope
 from homeassistant.components.recorder.db_schema import EventData, Events, EventTypes
-from homeassistant.util import dt as dt_util
 from homeassistant.components import websocket_api
 from homeassistant.components.websocket_api import messages
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.json import json_bytes
+from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import device_registry as dr
+from homeassistant.util import dt as dt_util
 
 from sqlalchemy import select
-from sqlalchemy.engine.row import Row
 from sqlalchemy.orm import Session
 
-from .const import DOMAIN, EVENT_DEVICE_WENT_OFFLINE, EVENT_DEVICE_CAME_ONLINE
+from .const import (
+    DOMAIN,
+    ENTITY_ATTR_TAG,
+    ENTITY_ATTR_HOST,
+    ENTITY_ATTR_DEVICE_ID,
+    ENTITY_ATTR_INTEGRATION_DOMAIN,
+    ENTITY_ATTR_INTEGRATION_NAME,
+    ENTITY_ATTR_INTEGRATION_CUSTOM_GROUP,
+    ENTITY_ATTR_STATE_SINCE,
+    ENTITY_ATTR_PINGS_FAILED,
+    ENTITY_TAG_PING_STATUS,
+    ENTITY_TAG_PINGS_FAILED_COUNT,
+    ENTITY_TAG_LAST_RESPONSE_TIME,
+    EVENT_DEVICE_WENT_OFFLINE,
+    EVENT_DEVICE_CAME_ONLINE,
+)
 
 @callback
 def async_setup(hass: HomeAssistant) -> None:
     """Set up the logbook websocket API."""
     websocket_api.async_register_command(hass, ws_get_events)
+    websocket_api.async_register_command(hass, ws_get_devices)
 
 def _ws_formatted_events(msg_id: int, events: list) -> bytes:
     """Convert events to json."""
@@ -70,7 +87,7 @@ def _get_events(hass: HomeAssistant, from_ts: int):
 async def ws_get_events(
     hass: HomeAssistant, connection: websocket_api.ActiveConnection, msg: dict[str, Any]
 ) -> None:
-    """Handle logbook get events websocket command."""
+    """Handle get events websocket command."""
     recorder = get_instance(hass)
 
     msg_id: int = msg["id"]
@@ -80,3 +97,93 @@ async def ws_get_events(
     events = await recorder.async_add_executor_job(_get_events, hass, hours_back_ts)
 
     connection.send_message(_ws_formatted_events(msg_id, events))
+
+@websocket_api.websocket_command(
+    {
+        vol.Required("type"): f"{DOMAIN}/get_devices",
+    }
+)
+@websocket_api.async_response
+async def ws_get_devices(
+    hass: HomeAssistant, connection: websocket_api.ActiveConnection, msg: dict[str, Any]
+) -> None:
+    """Handle get devices websocket command."""
+    msg_id: int = msg["id"]
+
+    entity_registry = er.async_get(hass)
+    device_registry = dr.async_get(hass)
+
+    result = {}
+
+    for entity_id, entry in entity_registry.entities.items():
+        if entry.platform != DOMAIN:
+            continue
+
+        entity_state = hass.states.get(entity_id)
+        if entity_state is None:
+            continue
+
+        if entity_state.attributes.get(ENTITY_ATTR_TAG) != ENTITY_TAG_PING_STATUS:
+            continue
+
+        device_id = entity_state.attributes.get(ENTITY_ATTR_DEVICE_ID)
+        device_entry = device_registry.devices.get(device_id)
+        device_name = device_entry.name_by_user or device_entry.name or "Unknown Device"
+
+        integration_domain = entity_state.attributes.get(ENTITY_ATTR_INTEGRATION_DOMAIN)
+        integration_name = entity_state.attributes.get(ENTITY_ATTR_INTEGRATION_NAME)
+        integration_custom_group = entity_state.attributes.get(ENTITY_ATTR_INTEGRATION_CUSTOM_GROUP)
+
+        host = entity_state.attributes.get(ENTITY_ATTR_HOST)
+
+        ping_status = {
+            "entity_id": entity_id,
+            "state": entity_state.state,
+            "unit_of_measurement": None,
+            "pings_failed": entity_state.attributes.get(ENTITY_ATTR_PINGS_FAILED)
+        }
+        ping_status_since_timestamp = entity_state.attributes.get(ENTITY_ATTR_STATE_SINCE)
+
+        # TODO: add config entry check and determine if optional sensors are enabled or not
+        last_response_time = None
+        pings_failed_count = None
+
+        for device_entity in er.async_entries_for_device(entity_registry, device_id):
+            device_entity_state = hass.states.get(device_entity.entity_id)
+            if device_entity_state is None:
+                continue
+            if device_entity_state.attributes.get(ENTITY_ATTR_TAG) == ENTITY_TAG_PINGS_FAILED_COUNT:
+                pings_failed_count = {
+                    "entity_id": device_entity.entity_id,
+                    "state": device_entity_state.state if ping_status.get("state") == "off" else None,
+                    "unit_of_measurement": None
+                }
+            elif device_entity_state.attributes.get(ENTITY_ATTR_TAG) == ENTITY_TAG_LAST_RESPONSE_TIME:
+                last_response_time = {
+                    "entity_id": device_entity.entity_id,
+                    "state": device_entity_state.state if ping_status.get("state") == "on" else None,
+                    "unit_of_measurement": device_entity.unit_of_measurement
+                }
+
+            if (
+                last_response_time is not None
+                and pings_failed_count is not None
+            ):
+                break
+
+        result[device_id] = {
+            "device_id": device_id,
+            "device_name": device_name,
+            "integration_domain": integration_domain,
+            "integration_name": integration_name,
+            "integration_custom_group": integration_custom_group,
+            "host": host,
+            "ping_status": ping_status,
+            "ping_status_since_timestamp": ping_status_since_timestamp,
+            "pings_failed_count": pings_failed_count,
+            "last_response_time": last_response_time,
+        }
+
+    connection.send_message(websocket_api.result_message(msg_id, {
+        'devices': result
+    }))
