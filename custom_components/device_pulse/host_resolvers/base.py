b9f4fd485898e33f5ea7fbed08e61340d776e92d
studiobts <info@studiobts.it>
2025-11-28 11:10:10 +0000

    Add device_configuration_url method to extract host from device configuration URL

--- custom_components/device_pulse/host_resolvers/base.py
+++ custom_components/device_pulse/host_resolvers/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from urllib.parse import urlparse
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers import device_registry as dr
@@ -10,3 +11,13 @@ class BaseHostResolver(ABC):
     @abstractmethod
     def resolve(config_entry: ConfigEntry, device: dr.DeviceEntry) -> str | None:
         """Return the Host for the given config entry."""
+
+
+    @staticmethod
+    def device_configuration_url(device: dr.DeviceEntry) -> str | None:
+        """Return the hostname of the configuration URL for the given device."""
+        if not device.configuration_url:
+            return None
+
+        parsed = urlparse(device.configuration_url)
+        return parsed.hostname
